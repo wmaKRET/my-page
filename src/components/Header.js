3e7fc6ddd99c65b3dcabf6e63db90876b93b28bd
wmaKRET <wkret91.2@gmail.com>
2022-07-15 19:46:53 +0200

    create Header layout and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,45 @@ import React from "react"
 import MyPhoto from "../assets/me.png"
 import CV from "../assets/cv.pdf"
 
+import { FaLinkedin, FaGithub } from "react-icons/fa"
+
 function Header() {
     return (
         <header className="header">
             <h5>Hello! My name is</h5>
             <h2>Wojciech</h2>
             <div className="header__content">
-                <img className="header__img" src={MyPhoto} alt="my photo" />
+                <div className="header__img">
+                    <img 
+                            className="header__img-photo" 
+                            src={MyPhoto} 
+                            alt="my photo" 
+                    />
+                    <div className="header__img-shadowtop"></div>
+                    <div className="header__img-shadowbottom"></div>
+                </div>
                 <div className="header__panel">
                     <a className="btn btn-primary" href={CV}>CV</a>
                     <a className="btn" href="#contact">Contact Me</a>
                 </div>
             </div>
+            <div className="header__socials">
+                <a 
+                    className="header__socials-icon" 
+                    href="https://www.linkedin.com/in/wojciech-kret/" 
+                    target="_blank"
+                >
+                    <FaLinkedin />
+                </a>
+                <a 
+                    className="header__socials-icon" 
+                    href="https://github.com/wmaKRET" 
+                    target="_blank"
+                >
+                    <FaGithub />
+                </a>
+            </div>
+            <p className="header__scroll">scroll down</p>
         </header>
     )
 }
