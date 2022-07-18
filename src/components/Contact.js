f7f7bd983fce1457ef5d6a1d50cd2b0c8a1b533f
wmaKRET <wkret91.2@gmail.com>
2022-07-18 13:52:25 +0200

    design Contact component and style it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,37 @@ function Contact(){
         <section className="contact" id="contact">
             <h5>how to</h5>
             <h2>Contact Me</h2>
+            <div className="contact__content">
+                <div className="contact__email">
+                    <h5>email</h5>
+                    <h4>wkret91.2@gmail.com</h4>
+                    <a href="mailto:wkret91.2@gmail.com">Send e-mail</a>
+                </div>
+                <form className="contact__form">
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Name"
+                        required
+                    />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email"
+                    />
+                    <textarea 
+                        name="message"
+                        placeholder="Message... (required)"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="btn"
+                    >
+                        Send Message
+                    </button>
+                </form>
+            </div>
         </section>
     )
 }
