4ce0337b826c3fc17d1c23d7879b4fef1aeb64b4
wmaKRET <wkret91.2@gmail.com>
2022-07-19 13:01:52 +0200

    basic structure for AboutMe component, separate scss files

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,6 @@ import React from "react"
 function AboutMe(){
     return (
         <section className="about" id="about">
-            <div className="about__shadowbox"></div>
             <h5>a little bit</h5>
             <h2>About Me</h2>
         </section>
