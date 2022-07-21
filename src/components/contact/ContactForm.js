a1c53214a51e9f00d79a8525c9706047c6c342db
wmaKRET <wkret91.2@gmail.com>
2022-07-21 08:51:19 +0200

    add ContactForm component logic to handle sending message

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,22 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 
 function ContactForm() {
+    const [contactData, setContactData] = useState(emptyContactData())
+
+    function emptyContactData(){
+        return {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setContactData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        // You got me! ;) it is not really sending a message
+        // on real page we could for example use EmailJS :)
+        console.log(`From ${contactData.email}`)
+        console.log(`${contactData.name} wrote: ${contactData.message}`)
+        setContactData(emptyContactData())
+        window.alert('Thanks for the message!')
+    }
+
     return (
-        <form className="contact__form">
+        <form onSubmit={handleSubmit} className="contact__form">
             <input
                 type="text"
                 name="name"
+                value={contactData.name}
+                onChange={handleChange}
                 placeholder="Name"
                 required
             />
             <input
                 type="email"
                 name="email"
+                value={contactData.email}
+                onChange={handleChange}
                 placeholder="Email"
             />
             <textarea
                 name="message"
-                placeholder="Message... (required)"
+                value={contactData.message}
+                onChange={handleChange}
+                placeholder="Message"
                 required
             />
             <button
